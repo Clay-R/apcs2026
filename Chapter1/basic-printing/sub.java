11f8a729f1e2fa4f2f28c2594b7bcdc25bb39333
Clay-R <rcriddell@stu.naperville203.org>
2026-08-20 14:04:04 -0500

    printing

---
+++ Chapter1/basic-printing/sub.java
@@ -0,0 +1,46 @@
+
+/**
+ * This is an assingment given to us when we had a sub to use system.out.print
+ *
+ * @Clay Riddell
+ * @1
+ */
+public class sub
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class sub
+     */
+    public sub()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    public static void main(String[] args)
+    {
+        /* this is a multi line comment. It can span multipule lines, like a paragraph
+         */
+        // this is a one line comment. It can only go one line.
+        System.out.print("This is a print statement");
+        System.out.println(". This is a new line of code but the same line of text.");
+        System.out.print("The previous line used ln, so this is a new line of code.");
+        
+        
+        
+    }
+}
